2ad8bd38bd6ffde2fe29996c2c3c6b7f966c87cb
feh-franc0 <fernandofv1110@gmail.com>
2023-09-06 21:48:41 -0300

    tirando o link e chamando uma funcao usando o useNavigate

--- src/components/ListPublications/index.tsx
+++ src/components/ListPublications/index.tsx
@@ -1,36 +1,35 @@
-import { Link } from "react-router-dom";
 import { PublicationsList, Publication } from "./styles";
 import ListPosts from "../../contexts/ListPosts";
 import { useContext } from "react";
 import { differenceInDays, parseISO } from 'date-fns';
 import { ReactMarkdown } from "react-markdown/lib/react-markdown";
+import { useNavigate } from 'react-router-dom'; // Importe useNavigate
 
 export function ListPublications() {
-
   const context = useContext(ListPosts);
+  const navigate = useNavigate(); // Obtenha a função navigate
 
-  if (!context) { throw new Error("ListPostsContext not provided"); }
+  if (!context) {
+    throw new Error("ListPostsContext not provided");
+  }
 
   const { myState } = context;
 
-  console.log("myState: ", myState)
+  console.log("myState: ", myState);
 
-  return(
+  const handlePublicationClick = (number: number) => {
+    navigate(`/post/${number}`);
+  };
+
+  return (
     <PublicationsList>
-    {myState.map((issue) => {
-      const parsedCreatedAt = parseISO(issue.created_at);
-      const today = new Date();
-      const daysSinceCreation = differenceInDays(today, parsedCreatedAt);
+      {myState.map((issue) => {
+        const parsedCreatedAt = parseISO(issue.created_at);
+        const today = new Date();
+        const daysSinceCreation = differenceInDays(today, parsedCreatedAt);
 
-      return (
-        <Publication key={issue.id}>
-          <Link
-            to={`/post/${issue.number}`}
-            style={{
-              color: "inherit",
-              textDecoration: "none",
-            }}
-          >
+        return (
+          <Publication key={issue.id} onClick={() => handlePublicationClick(issue.number)}>
             <h2>{issue.title}</h2>
             <div>há {daysSinceCreation} {daysSinceCreation === 1 ? 'dia' : 'dias'}.</div>
             <p>
@@ -38,10 +37,9 @@ export function ListPublications() {
                 {issue.body.length > 300 ? issue.body.slice(0, 100 - 3) + "..." : issue.body}
               </ReactMarkdown>
             </p>
-          </Link>
-        </Publication>
-      );
-    })}
+          </Publication>
+        );
+      })}
     </PublicationsList>
-  )
+  );
 }
